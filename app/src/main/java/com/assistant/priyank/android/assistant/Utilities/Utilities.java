6e3240e09d90e3cc9e352fcdd495df8bfbd2ad08
Priyank Mandavawala <primanda@microsoft.com>
2017-05-01 23:31:58 +0530

    Swipe to refresh + Network check

--- app/src/main/java/com/assistant/priyank/android/assistant/Utilities/Utilities.java
+++ app/src/main/java/com/assistant/priyank/android/assistant/Utilities/Utilities.java
@@ -1,5 +1,9 @@
 package com.assistant.priyank.android.assistant.Utilities;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
 import com.assistant.priyank.android.assistant.Models.StockDataModel;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -16,5 +20,12 @@ public class Utilities {
         GsonBuilder builder = new GsonBuilder();
         Gson gson = builder.create();
         return gson.fromJson(reponseString, StockDataModel[].class);
+    }
+
+    public static boolean IsConnectedToInternet(Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        //should check null because in airplane mode it will be null
+        return (netInfo != null && netInfo.isConnected());
     }
 }
